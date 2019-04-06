017c671b7452450d75a6641679eacf281a81b644
washoe <b.oliver@optusnet.com.au>
2019-04-07 09:35:49 +1000

    Simple form with collapsible sections

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <DetailsForm msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import DetailsForm from './components/DetailsForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    DetailsForm
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ src/components/DetailsForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <form  @submit.prevent="handleSubmit">
+    <fieldset>
+      <details>
+        <summary>
+          <legend>Part 1</legend>
+        </summary>
+        <label for="input1">Input 1</label>
+        <input type="text" id="input1" name="input1">
+      </details>
+    </fieldset>
+    <fieldset>
+      <details>
+        <summary>
+          <legend>Part 2</legend>
+        </summary>
+        <label for="input2">Input 2</label>
+        <input type="date" id="input2" name="input2">
+      </details>
+    </fieldset>
+    <fieldset>
+      <button type="submit">Submit</button>
+    </fieldset>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "DetailsForm",
+  methods: {
+    handleSubmit: function (event) {
+      
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+summary {
+  display: flex;
+  align-items: center;
+}
+</style>
