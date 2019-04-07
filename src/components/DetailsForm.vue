afafedcbcfe695ea7fd2e733d81293d8bf567511
washoe <b.oliver@optusnet.com.au>
2019-04-07 18:19:42 +1000

    Add css only alternative
    Use :focus-within

--- src/components/DetailsForm.vue
+++ src/components/DetailsForm.vue
@@ -1,5 +1,9 @@
 <template>
+<section>
+<h1>Collapsible forms</h1>
+
   <form @submit.prevent="handleSubmit">
+    <h2>Using details element and some js</h2>
     <FormSection section-title="Part 1">
         <label for="input1">Input 1</label>
         <input type="text" id="input1" name="input1">
@@ -19,14 +23,39 @@
       <button type="submit">Submit</button>
     </fieldset>
   </form>
+
+    <form @submit.prevent="handleSubmit">
+    <h2>Using css only</h2>
+    <FormSection2 section-title="Part 1">
+        <label for="input1">Input 1</label>
+        <input type="text" id="input1" name="input1">
+    </FormSection2>
+    <FormSection2 section-title="Part 2">
+        <label for="input2">Input 2</label>
+        <input type="date" id="input2" name="input2">
+    </FormSection2>
+    <FormSection2 section-title="Part 3">
+      <label for="input3">Input 3</label>
+      <select id="input3" name="input3">
+        <option>option 1</option>
+        <option>option 2</option>
+      </select>
+    </FormSection2>
+    <fieldset>
+      <button type="submit">Submit</button>
+    </fieldset>
+  </form>
+  </section>
 </template>
 
 <script>
 import FormSection from "./FormSection";
+import FormSection2 from "./FormSection2";
 export default {
   name: "DetailsForm",
   components: {
-    FormSection
+    FormSection,
+    FormSection2
   },
   methods: {
     handleSubmit: function() {}
---
+++ src/components/FormSection2.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <header>{{sectionTitle}}<button>Edit</button></header>
+    <fieldset>
+      <slot/>
+    </fieldset>
+  </section>
+</template>
+<script>
+export default {
+  name: "FormSection2",
+  props: {
+    sectionTitle: String
+  }
+};
+</script>
+
+<style scoped>
+section:focus-within fieldset {
+  display: block;
+}
+section:not(:focus-within) fieldset {
+  display: none;
+}
+</style>
